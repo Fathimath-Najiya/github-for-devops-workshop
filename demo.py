f90a570d4490badf4029f64d060060924e5b6ad3
Najiya <najiyanazheer@gmail.com>
2026-09-18 23:56:21 +0530

    new demo

--- demo.py
+++ demo.py
@@ -1,5 +1,13 @@
-def my_function():
+"""
+Demo module containing basic mathematical helper functions.
+"""
 
+
+def my_function():
+  
+    """
+    Returns a static integer value of 5.
+    """
     a = 5
     return a
 
