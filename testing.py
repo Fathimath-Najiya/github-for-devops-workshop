73b51182fcc9891806ccae51e0c7e4f20dbc6ff4
Najiya <najiyanazheer@gmail.com>
2026-09-18 23:45:40 +0530

    testing

--- testing.py
+++ testing.py
@@ -1,8 +1,11 @@
-def hello():
-    """
-    this is demo
-    """
-    return "Hello Friends"
+"""
+Module for testing basic greetings.
+""" 
 
+def hello():
+"""
+Returns a standard friendly greeting string.
+"""
+return "Hello Friends" 
 
 hello()
